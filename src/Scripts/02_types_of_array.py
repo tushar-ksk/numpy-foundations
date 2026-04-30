bda3a819031079acfe21fd346e35434cbd206b0f
nonit-kaushik <tusharsharma1328@gmail.com>
2026-04-30 23:31:36 +0530

    add types of array

---
+++ src/Scripts/02_types_of_array.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Zeros Array
+arr_z = np.zeros((3,3,3))
+print(arr_z)
+
+# Ones Array
+arr_o = np.ones((3,2,3))
+print(arr_o)
+
+# random Array
+arr_r = np.random.random((2,3))
+print(arr_r)
