0bec2b2c714644480874cce29b05c50c5b505514
nonit-kaushik <tusharsharma1328@gmail.com>
2026-04-30 23:37:35 +0530

    add array dimensions

---
+++ src/Scripts/03_array_dimensions.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# number of dimensions in an array is called its rank
+# 1D Array
+arr_1d = np.array([1,2,3,4])
+print("1D Array\n", arr_1d)
+
+# 2D Array
+arr_2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("2D Array\n", arr_2d)
+
+# 3D Array
+arr_3d = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print("3D Array\n", arr_3d)
